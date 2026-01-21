20fc8847aa7a8125035927b2af27b39477a5a398
Somm <sommsyai@gmail.com>
2026-01-20 23:06:57 -0800

    Update error message for missing index.html

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     fs.readFile(path.join(__dirname, 'index.html'), (err, content) => {
       if (err) {
         res.writeHead(500);
-        res.end('Error loading index.html. Did you upload it?');
+        res.end('Error: index.html missing from Git repo.');
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(content);
@@ -48,8 +48,12 @@ const server = http.createServer((req, res) => {
         }
       });
     });
+    apiReq.on('error', e => console.error(e));
     apiReq.end(data); // Send request
-  } else {
+  } 
+  
+  // 3. Fallback
+  else {
     res.writeHead(404);
     res.end("Not Found");
   }
